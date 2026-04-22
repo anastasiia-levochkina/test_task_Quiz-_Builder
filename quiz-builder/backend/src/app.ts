0d07c3e0d5ea13bab61e6c30835d64cdcef0197c
Anastasiia Levochkina <nastya.mixaylova98@gmail.com>
2026-04-22 21:19:26 +0300

    simplify cors for deployment

--- quiz-builder/backend/src/app.ts
+++ quiz-builder/backend/src/app.ts
@@ -7,11 +7,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 
